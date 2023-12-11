93ddbcc29569ddf67393957bdb0f81deb48593d6
Hansdika Nugroho <hansdikanugroho.1@gmail.com>
2023-12-11 21:55:36 +0700

    first commit

---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  <link href="../../css/dist/output.css" rel="stylesheet">
+</head>
+<body>
+
+  @include('navbar')
+
+  <div class="wrapper">
+    @yield('content')
+
+  </div>
+
+
+</body>
+</html>
---
+++ resources/views/layout/navbar.blade.php
@@ -0,0 +1,9 @@
+<nav>
+  
+  <nav>
+    <a href="{{ route('home') }}">Home</a>
+    <a href="{{ route('about') }}">About</a>
+    <a href="{{ route('contact') }}">Contact</a>
+  </nav>
+  
+</nav>
